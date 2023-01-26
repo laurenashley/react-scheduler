30cff8c8b08910d5503fe95fc561886ff7e9305f
Lauren Ashley <la.zaubi@gmail.com>
2023-01-26 10:53:01 -0800

    add css corrections

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import classNames from "classnames";
 
-import "components/InterviewerListItem";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem (props) {
+  const liClass = classNames('interviewers__item', {
+    'interviewers__item--selected': props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li 
+      className={liClass} 
+      key={props.id}
+      onClick={props.setInterviewer}
+    >
     <img
       className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
+      src={props.avatar}
+      alt={props.name}
     />
-      Sylvia Palmer
+      {(props.name) && props.name}
     </li>
   );
 }
