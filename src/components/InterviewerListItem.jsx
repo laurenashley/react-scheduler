a9ea1ab341faf6cccd7340b5805ce2ab7eaf53c7
Lauren Ashley <la.zaubi@gmail.com>
2023-01-26 10:33:11 -0800

    add provided code from lesson for new component

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "components/InterviewerListItem";
+
+export default function InterviewerListItem (props) {
+  return (
+    <li className="interviewers__item">
+    <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+      Sylvia Palmer
+    </li>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -67,3 +67,37 @@ storiesOf("DayList", module)
   .add("Wednesday", () => (
       <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+  
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
