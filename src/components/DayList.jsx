77952b8b52a5d86ed1f0b2e2b1df3c4dd7c53682
Lauren Ashley <la.zaubi@gmail.com>
2023-01-25 22:11:39 -0800

    finish testing isolated component, start the parent component

---
+++ src/components/DayList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+function DayList () {
+  return (
+    <ul></ul>
+  );
+}
+
+export default DayList;
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -3,15 +3,26 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const dayClass = classNames('li', 'day-list__item', {
-    'day-list__item--selected': props.selected === 'true',
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
+
+  const formatSpots = () => {
+    const numSpots = props.spots;
+    return (numSpots === 0 )
+            ? 'no spots '
+            : (numSpots === 1)
+              ? '1 spot '
+              : `${numSpots} spots `;
+  };
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
+
+export default DayListItem;
