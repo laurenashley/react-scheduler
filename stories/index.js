2e9756b5beb57e2df3f0772a02326231b66a25ee
Lauren Ashley <la.zaubi@gmail.com>
2023-01-29 20:51:00 -0800

    add new components

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty.jsx";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error.jsx";
 
 storiesOf("Button", module)
   .addParameters({
@@ -162,4 +163,5 @@ storiesOf("Appointment", module)
                       onConfirm={action("onConfirm")}
                       onCancel={action("onCancel")}
                      />)
-  .add("Status", () => <Status message="Deleting" />);
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => <Error message="Could not delete appointment." />);
