d4024ce7b20a36c409b1fd9c2fdc91e4bc1be18f
Lauren Ashley <la.zaubi@gmail.com>
2023-02-02 14:37:02 -0800

    complete helpres testing lesson

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,12 @@
 export function getAppointmentsForDay(state, day) {
   const dayData = state.days.find(thisDay => thisDay.name === day);
-  console.log('dayDta::::::: ', dayData.appointments);
-  const appointments = [];
+  const appointmentsArray = [];
 
-  for (const appointment in dayData.appointments) {
-    console.log('thisAppntment:::: ', appointment)
-    // iterate over state.appointments, adding dayData's appnt to appoitntments array
-    appointments.push(appointment);
+  if (dayData) {
+    for (const appointment of dayData.appointments) {
+      appointmentsArray.push(state.appointments[appointment]);
+    }
   }
 
-  return appointments;
+  return appointmentsArray;
 }
