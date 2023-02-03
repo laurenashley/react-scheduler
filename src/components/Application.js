a21390438e3e26a323501f17e0e5abab1485c0c9
Lauren Ashley <la.zaubi@gmail.com>
2023-02-03 11:55:34 -0800

    progress adding CREATE mode transition

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -4,17 +4,34 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <Fragment>
       <Header time={props.time} />
       <article className="appointment">
-      {
-        props.interview 
-          ? <Show student={props.interview.student} interviewer={props.interview.interviewer} />
-          : <Empty />
-      }
+      { mode === EMPTY && <Empty onAdd={() => transition(CREATE)} /> }
+      { mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      { mode === CREATE && (
+        <Show
+          student="Name"
+          interviewer=""
+        />
+      )}
       </article> 
     </Fragment>
   );
