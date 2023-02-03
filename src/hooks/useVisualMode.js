e17872c74d7a4b992e1bb7f12ec2e87100c89081
Lauren Ashley <la.zaubi@gmail.com>
2023-02-03 07:46:27 -0800

    add custom hook for navigating appointment modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,21 +3,15 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  // const historyArray = [];
   
   function transition(newMode) {
-    console.log('newMode: ',newMode);
     setMode(newMode);
-    setHistory([prev, newMode]);
-    console.log('history after push : ', history);
+    history.push(newMode);
   }
 
-  // console.log('history before pop: ', historyArray, historyArray.length);
   function back() {
-    // console.log('history before pop: ', historyArray, historyArray.length);
-    // historyArray.pop();
-    // console.log('history after pop: ', historyArray, historyArray.length);
-    // setMode(historyArray[historyArray.length - 1]);
+    history.pop();
+    setMode(history[history.length - 1]);
   }
 
   return { mode, transition, back };
