f8915c84a8fba22e9ac4e8f8b26a2fe76532654a
Lauren Ashley <la.zaubi@gmail.com>
2023-02-02 17:11:24 -0800

    combine state

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,9 +45,19 @@ const appointments = {
   }
 };
 
+
 export default function Application(props) {
-  const [days, setDays ] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+  };
+
 
   useEffect(() => {
     axios.get('/api/days')
@@ -67,8 +77,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          day={state.day}
           onChange={setDay}
         />
       </nav>
