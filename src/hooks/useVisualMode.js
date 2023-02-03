f9636520aaecfd69f87eea9262a38366cd24f4aa
Lauren Ashley <la.zaubi@gmail.com>
2023-02-02 22:08:05 -0800

    add third mode progress

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,23 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  // const historyArray = [];
   
   function transition(newMode) {
+    console.log('newMode: ',newMode);
     setMode(newMode);
+    setHistory([prev, newMode]);
+    console.log('history after push : ', history);
   }
 
-  return { mode, transition };
+  // console.log('history before pop: ', historyArray, historyArray.length);
+  function back() {
+    // console.log('history before pop: ', historyArray, historyArray.length);
+    // historyArray.pop();
+    // console.log('history after pop: ', historyArray, historyArray.length);
+    // setMode(historyArray[historyArray.length - 1]);
+  }
+
+  return { mode, transition, back };
 }
