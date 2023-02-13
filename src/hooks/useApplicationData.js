4c70bf8a7cd6eb107f8f4da915ee52cb1c1c9717
Lauren Ashley <la.zaubi@gmail.com>
2023-02-13 12:46:51 -0800

    update days state as array, setState bugfixed. Next refigure approach to update spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -23,21 +23,25 @@ export default function useApplicationData() {
     return [ appointment, appointments ];
   };
 
-  const updateSpots = (cancel) => {
-    // find day by appointmentID
-    const day = state.days.find(day => day.name === "Monday");
+  const updateSpots = (appointments) => {
+    // find day 
+    const day = state.days.find(day => day.name === state.day);
     const dayID = day.id - 1;
 
+    // count appointments that have null interview
+    const spotsCount = state.days[dayID].appointments.reduce();
+    // iterate through state.appointments per state.days.appointments
+    // Count app's with null appointment
+
+
     const dayState = {
-      ...state.days[dayID],
-      spots: cancel ? day.spots + 1 : day.spots - 1
+      ...state.days[dayID]
+      // spots: cancel ? day.spots + 1 : day.spots - 1
     };
     console.log('dayState: ', dayState);
 
-    const days = {
-      ...state.days,
-      [dayID]: dayState
-    };
+    const days = [ ...state.days ];
+    days[dayID] = dayState;
     console.log('new Days state: ', days);
 
     return days;
@@ -45,9 +49,7 @@ export default function useApplicationData() {
 
   function bookInterview(id, interview) {
     const [ appointment, appointments ] = packAppointments(id, interview);
-    const days = updateSpots(false);
-
-    setState({ ...state, appointments, days });
+    const days = updateSpots(appointments);
 
     return (
       axios.put(`/api/appointments/${id}`, appointment)
@@ -60,9 +62,7 @@ export default function useApplicationData() {
 
   function cancelInterview(id) {
     const [ appointment, appointments ] = packAppointments(id);
-    const days = updateSpots(true);
-
-    setState({ ...state, appointments, days });
+    const days = updateSpots(appointments);
 
     return (
       axios.delete(`/api/appointments/${id}`)
@@ -81,8 +81,8 @@ export default function useApplicationData() {
       axios.get('/api/interviewers')
     ])
       .then(all => {
-        console.log(all[0]);
-        console.log(all[1]);
+        console.log('Days Data: ', all[0]);
+        console.log('Appointments Data: ', all[1]);
         setState(prev => ({...prev, 
           days: all[0].data, 
           appointments: all[1].data,
