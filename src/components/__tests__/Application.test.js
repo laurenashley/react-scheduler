c3a308b0b0f56d733dd8a4f34be40e277635ba46
Lauren Ashley <la.zaubi@gmail.com>
2023-02-18 16:20:03 -0800

    chain click event and expect for app test, clean up code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -9,5 +9,9 @@ afterEach(cleanup);
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"));
+  return waitForElement(() => getByText("Monday"))
+          .then(() => {
+            fireEvent.click(getByText("Tuesday"));
+            expect(getByText(/Leopold Silvers/i)).toBeInTheDocument();
+          });
 });
