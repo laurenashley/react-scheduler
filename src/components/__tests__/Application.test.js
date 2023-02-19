6ad6e8018ab9f1149431c67b92c57e41407499fe
Lauren Ashley <la.zaubi@gmail.com>
2023-02-18 16:51:18 -0800

    add new test, use react query getByText

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,17 +1,34 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  getByText,
+  prettyDOM
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"))
+            .then(() => {
+              fireEvent.click(getByText("Tuesday"));
+              expect(getByText(/Leopold Silvers/i)).toBeInTheDocument();
+            });
+  });
 
-  return waitForElement(() => getByText("Monday"))
-          .then(() => {
-            fireEvent.click(getByText("Tuesday"));
-            expect(getByText(/Leopold Silvers/i)).toBeInTheDocument();
-          });
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, /Archie Cohen/i));
+    
+    console.log(prettyDOM(container));
+  });
 });
