88e5ff480bdcfccb48b63ca78d82a8c3161e3a19
Lauren Ashley <la.zaubi@gmail.com>
2023-02-21 16:09:53 -0800

    turn all tests back on, add delete method for mock axios

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -80,7 +80,13 @@ export default {
       });
     }
   }),
-  put: jest.fn(url => {
+  put: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
+  }),
+  delete: jest.fn(() => {
     return Promise.resolve({
       status: 204,
       statusText: "No Content"
