81e45015a12b6f6477722ac42a521e071c48883b
Lauren Ashley <la.zaubi@gmail.com>
2023-02-23 14:26:02 -0800

    edit interview test added, in progress

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,11 +1,38 @@
 describe("Appointment", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+
     cy.visit("/");
+
     cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
   
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    
+    // Start here: add hover selector to get edit button
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 });
