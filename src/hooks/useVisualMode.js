0e3fac2a30cda4974217cf5f15290b712145e92c
Lauren Ashley <la.zaubi@gmail.com>
2023-03-06 20:51:06 -0800

    fix state management bug, refactor transition and back blocks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,33 +1,32 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [ mode, setMode ] = useState(initial);
   const [ history, setHistory ] = useState([initial]);
-
-  function removeLastModeInHistory() {
-    // Remove last mode as setup to replace it 
-    const newHistory = [...history].pop();
-    setHistory(newHistory);
-  }
   
-  function transition(newMode, replace=false) {
-    if (replace) {
-      removeLastModeInHistory();
-    }
-    setMode(newMode);
-    history.push(newMode);
+  function transition(newMode, replace=false, back=false) {
+    setHistory(prev => {
+      const copy = [...prev];
+
+      /** removes last mode in history with current mode */
+      /** ie. for modes coming after Saving or Deleting */
+      if (replace) copy.pop();
+      if (!back) copy.push(newMode);
+
+      return copy;
+    });
   }
 
   function back() {
-    if (history.length >= 1) {
-      removeLastModeInHistory();
-      setMode(history[history.length - 1]);
+    if (history.length > 1) {
+      transition(history[history.length - 1], true, true);
     } 
     
-    if (history.length < 1) {
+    if (history.length <= 1) {
+      /** If nothing to go back to in history set to initial mode */
       transition(initial);
     }
   }
 
-  return { mode, transition, back };
+  /** pass last mode in history array via mode key */
+  return { mode: history[history.length - 1], transition, back };
 }
